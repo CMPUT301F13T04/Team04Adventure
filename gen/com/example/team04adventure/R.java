4b8eb70c400f693d69baf3ea274d6be93cf45e9c
Mike Kmicik <Mike@uwsclient-145-109.uws.ualberta.ca>
2013-10-29 13:17:08 -0600

    Adds the frag viewer xml and class

--- gen/com/example/team04adventure/R.java
+++ gen/com/example/team04adventure/R.java
@@ -39,19 +39,22 @@ public final class R {
         public static final int username=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_sign_up=0x7f030001;
-        public static final int activity_story_intro=0x7f030002;
+        public static final int activity_fragment_viewer=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_sign_up=0x7f030002;
+        public static final int activity_story_intro=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int sign_up=0x7f070001;
-        public static final int story_intro=0x7f070002;
+        public static final int fragment_viewer=0x7f070000;
+        public static final int main=0x7f070001;
+        public static final int sign_up=0x7f070002;
+        public static final int story_intro=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_fragment_viewer=0x7f050005;
         public static final int title_activity_sign_up=0x7f050003;
         public static final int title_activity_story_intro=0x7f050004;
     }
---
+++ src/com/example/team04adventure/FragmentViewer.java
@@ -0,0 +1,22 @@
+package com.example.team04adventure;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class FragmentViewer extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_fragment_viewer);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.fragment_viewer, menu);
+		return true;
+	}
+
+}

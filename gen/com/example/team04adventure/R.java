b8b3277b0fb346d71d398ad66faa068f677c7c20
ftyau <ftyau@ualberta.ca>
2013-10-29 21:32:08 -0600

    Initial edit screen and camera handling

--- gen/com/example/team04adventure/R.java
+++ gen/com/example/team04adventure/R.java
@@ -24,50 +24,65 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int AddFrag=0x7f080009;
-        public static final int Download=0x7f08000b;
-        public static final int Edit=0x7f08000a;
-        public static final int Play=0x7f080008;
-        public static final int StoryAuthor=0x7f080007;
-        public static final int StoryTitle=0x7f080006;
-        public static final int action_settings=0x7f080010;
-        public static final int button1=0x7f08000c;
-        public static final int login=0x7f080001;
-        public static final int offline=0x7f080002;
-        public static final int register=0x7f080005;
-        public static final int registerusername=0x7f080004;
-        public static final int signup=0x7f080003;
-        public static final int storyauthor=0x7f08000e;
-        public static final int storyfragments=0x7f08000f;
-        public static final int storytitle=0x7f08000d;
-        public static final int username=0x7f080000;
+        public static final int AddFrag=0x7f08000f;
+        public static final int Download=0x7f080011;
+        public static final int Edit=0x7f080010;
+        public static final int Play=0x7f08000e;
+        public static final int StoryAuthor=0x7f08000d;
+        public static final int StoryTitle=0x7f08000c;
+        public static final int action_settings=0x7f080016;
+        public static final int button1=0x7f080012;
+        public static final int camera=0x7f080003;
+        public static final int frag_body=0x7f080001;
+        public static final int frag_title=0x7f080000;
+        public static final int link=0x7f080004;
+        public static final int login=0x7f080007;
+        public static final int offline=0x7f080008;
+        public static final int register=0x7f08000b;
+        public static final int registerusername=0x7f08000a;
+        public static final int save=0x7f080005;
+        public static final int signup=0x7f080009;
+        public static final int storyauthor=0x7f080014;
+        public static final int storyfragments=0x7f080015;
+        public static final int storytitle=0x7f080013;
+        public static final int upload=0x7f080002;
+        public static final int username=0x7f080006;
     }
     public static final class layout {
-        public static final int activity_fragment_viewer=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_offline_story_list=0x7f030002;
-        public static final int activity_online_story_list=0x7f030003;
-        public static final int activity_sign_up=0x7f030004;
-        public static final int activity_story_intro=0x7f030005;
-        public static final int storylistlayout=0x7f030006;
+        public static final int activity_edit_fragment=0x7f030000;
+        public static final int activity_fragment_viewer=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_offline_story_list=0x7f030003;
+        public static final int activity_online_story_list=0x7f030004;
+        public static final int activity_sign_up=0x7f030005;
+        public static final int activity_story_intro=0x7f030006;
+        public static final int storylistlayout=0x7f030007;
     }
     public static final class menu {
-        public static final int fragment_viewer=0x7f070000;
-        public static final int main=0x7f070001;
-        public static final int offline_story_list=0x7f070002;
-        public static final int online_story_list=0x7f070003;
-        public static final int sign_up=0x7f070004;
-        public static final int story_intro=0x7f070005;
+        public static final int edit=0x7f070000;
+        public static final int fragment_viewer=0x7f070001;
+        public static final int main=0x7f070002;
+        public static final int offline_story_list=0x7f070003;
+        public static final int online_story_list=0x7f070004;
+        public static final int sign_up=0x7f070005;
+        public static final int story_intro=0x7f070006;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int camera=0x7f050006;
+        public static final int frag_body=0x7f050004;
+        public static final int frag_title=0x7f050003;
         public static final int hello_world=0x7f050002;
-        public static final int title_activity_fragment_viewer=0x7f050005;
-        public static final int title_activity_offline_story_list=0x7f050006;
-        public static final int title_activity_online_story_list=0x7f050007;
-        public static final int title_activity_sign_up=0x7f050003;
-        public static final int title_activity_story_intro=0x7f050004;
+        public static final int link=0x7f050007;
+        public static final int save=0x7f050008;
+        public static final int title_activity_edit_fragment=0x7f05000e;
+        public static final int title_activity_fragment_viewer=0x7f05000b;
+        public static final int title_activity_offline_story_list=0x7f05000c;
+        public static final int title_activity_online_story_list=0x7f05000d;
+        public static final int title_activity_sign_up=0x7f050009;
+        public static final int title_activity_story_intro=0x7f05000a;
+        public static final int upload=0x7f050005;
     }
     public static final class style {
         /** 
---
+++ src/com/example/team04adventure/EditFragment.java
@@ -0,0 +1,90 @@
+package com.example.team04adventure;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class EditFragment extends Activity {
+
+	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+	
+	private Button uploadButton;
+	private Button cameraButton;
+	private Button linkButton;
+	private Button saveButton;
+		
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_edit_fragment);
+		
+		uploadButton = (Button) findViewById(R.id.upload);
+		cameraButton = (Button) findViewById(R.id.camera);
+		linkButton = (Button) findViewById(R.id.link);
+		saveButton = (Button) findViewById(R.id.save);
+		
+		uploadButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		cameraButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				openCamera();
+			}
+		});
+		
+		linkButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		saveButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	}
+	
+	public void openCamera() {
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		startActivityForResult(takePictureIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+	}
+	
+	private void handleSmallCameraPhoto(Intent intent) {
+	    Bundle extras = intent.getExtras();
+	    Bitmap mImageBitmap = (Bitmap) extras.get("data");
+	    // mImageBitmap is the image from the camera
+	    
+	}
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		handleSmallCameraPhoto(data);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.edit, menu);
+		return true;
+	}
+
+}

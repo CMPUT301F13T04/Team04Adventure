557f28ddebd23f7df437c2dcc7c6d4f79ff6bbf4
Mike Kmicik <mkmicik@ualberta.ca>
2013-11-01 17:56:32 -0600

    Update SignUp.java

--- src/com/example/team04adventure/SignUp.java
+++ src/com/example/team04adventure/SignUp.java
@@ -1,41 +1,73 @@
 package com.example.team04adventure;
 
 
-import com.example.team04adventure.R;
-import com.example.team04adventure.R.id;
-import com.example.team04adventure.R.layout;
-import com.example.team04adventure.R.menu;
-
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUp extends Activity {
 
 	private EditText regUsernameText; 
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sign_up);
 		regUsernameText = (EditText) findViewById(R.id.username);
 		Button Register = (Button) findViewById(R.id.register);
-		
+
 		Register.setOnClickListener(new View.OnClickListener()
 		{
-				
+
 			public void onClick(View v)
 			{
-//ADD CHECK TO MAKE SURE USERNAME DOES NOT ALREADY EXIST
-//ADD STUFF HERE TO PUT THE USERNAME IN THE DB - from regUsernameText
-				Intent i = new Intent(SignUp.this, MainActivity.class);
-				startActivity(i);
+				String Uname = regUsernameText.getText().toString();
+
+				Authenticator auth = new Authenticator();
+				int yayOrNay = auth.authenticate(Uname);
+
+				String message = "";
+
+				switch (yayOrNay) {
+				case -1:
+					message = "Connection Failed.";
+					break;
+				case 0:
+					int j = addUsername(Uname);
+					if (j == 1) {
+						Intent i = new Intent(SignUp.this, MainActivity.class);
+						startActivity(i);
+					}
+					else {
+						message = "Add Failed.";
+					}
+					break;
+				case 1:
+					message = "Username already existed";
+					break;
+				}
+				
+				Toast.makeText(getBaseContext(), message, Toast.LENGTH_LONG).show();
+				
 			}
 		});
+	}
+	
+	private int addUsername(String name) {
+		User u = new User(name);
+		JSONparser j = new JSONparser();
+		try {
+			j.storeUser(u);
+		}
+		catch (Exception e){
+			return -1;
+		}
+		return 1;
 	}
 
 	@Override

1b33f47b36345b93a72bb1a88d1727a7f02f9bd5
mkmicik <Mike@uwsclient-145-230.uws.ualberta.ca>
2013-11-02 15:11:51 -0600

    Added some test case frameworks

---
+++ src/com/example/team04adventure/JSONparserTest.java
@@ -0,0 +1,25 @@
+package com.example.team04adventure;
+
+public class JSONparserTest implements TestCase{
+
+	@Override
+	public void setUp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void tearDown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ru() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
---
+++ src/com/example/team04adventure/TestCase.java
@@ -0,0 +1,11 @@
+package com.example.team04adventure;
+
+public interface TestCase {
+
+	public void setUp();
+	
+	public void tearDown();
+	
+	public void ru();
+	
+}

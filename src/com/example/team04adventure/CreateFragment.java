d1a8e9b6f7c312da92566028430858a05345ab9a
chrismoulds <cmoulds@ualberta.ca>
2013-11-02 12:26:00 -0600

    New commit

---
+++ src/com/example/team04adventure/CreateFragment.java
@@ -0,0 +1,35 @@
+package com.example.team04adventure;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.text.method.ScrollingMovementMethod;
+import android.view.Menu;
+import android.widget.EditText;
+
+public class CreateFragment extends Activity {
+
+	private EditText title, body;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_create_fragment);
+		
+		title = (EditText)findViewById(R.id.ftitle);
+		body = (EditText)findViewById(R.id.fbody);
+		title.setMovementMethod(new ScrollingMovementMethod());
+		body.setMovementMethod(new ScrollingMovementMethod());
+		
+		
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.create, menu);
+		return true;
+	}
+
+}

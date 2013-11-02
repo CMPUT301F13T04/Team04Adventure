304070bf1e905c109639cd6292844a7640e5a1ba
Chris Moulds <cmoulds@ualberta.ca>
2013-11-02 16:07:53 -0600

    Update OnlineStoryList.java

--- src/com/example/team04adventure/OnlineStoryList.java
+++ src/com/example/team04adventure/OnlineStoryList.java
@@ -1,9 +1,15 @@
 package com.example.team04adventure;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Locale;
+import java.util.Random;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -14,7 +20,12 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
+
 import com.example.team04adventure.*;
 
 public class OnlineStoryList extends FragmentActivity implements
@@ -34,11 +45,13 @@ public class OnlineStoryList extends FragmentActivity implements
 	 * The {@link ViewPager} that will host the section contents.
 	 */
 	ViewPager mViewPager;
-
+	String Uname;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_online_story_list);
+		Bundle extras = getIntent().getExtras();
+		Uname = extras.getString("uname");
 
 		// Set up the action bar.
 		final ActionBar actionBar = getActionBar();
@@ -74,8 +87,108 @@ public class OnlineStoryList extends FragmentActivity implements
 					.setText(mSectionsPagerAdapter.getPageTitle(i))
 					.setTabListener(this));
 		}
+		
+		final ListView storyListView = (ListView) findViewById(R.id.cachedlist);
+		ArrayList<Story> storylist = new ArrayList<Story>();
+		StorageManager sm = new StorageManager(this);
+		
+		/** Open DB connection and retrieve all of 
+		    the cached stories. **/
+		
+		storylist = sm.getAll();
+		
+		
+		storyListView.setAdapter(new StoryListAdapter(this, storylist));
+		storyListView.setOnItemClickListener(new OnItemClickListener() {
+        
+			/** When a story is selected **/
+			@Override
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+				Story s = (Story) storyListView.getItemAtPosition(position);
+                
+				Intent intent = new Intent(getApplicationContext(), StoryIntro.class);
+				intent.putExtra("uname", Uname);
+				intent.putExtra("id", s.getId());
+        		startActivity(intent);
+             }
+
+        });
+		
+		
+		final ListView ostoryListView = (ListView) findViewById(R.id.list);
+		ArrayList<Story> ostorylist = new ArrayList<Story>();
+		
+		
+		JSONparser jp = new JSONparser();
+		
+		ArrayList<Story> stories = jp.getAll();
+		
+
+		storyListView.setAdapter(new StoryListAdapter(this, stories));
+		storyListView.setOnItemClickListener(new OnItemClickListener() {
+        
+			/** When a story is selected **/
+			@Override
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+				Story s = (Story) ostoryListView.getItemAtPosition(position);
+                Intent intent = new Intent(getApplicationContext(), OnlineStoryIntro.class);
+        		intent.putExtra("uname", Uname);
+                intent.putExtra("id", s.getId());
+        		startActivity(intent);
+             }
+
+        });
+	
+	
+		
+		
 	}
 
+	
+	public void addStory(View view){
+		
+		AlertDialog.Builder adb = new AlertDialog.Builder(this);
+		final EditText input = new EditText(this); 
+		 adb.setView(input);
+		
+		adb.setTitle("Story Title");
+		
+		adb.setPositiveButton("Create", new DialogInterface.OnClickListener() {  
+		    public void onClick(DialogInterface dialog, int whichButton) {  
+		        Story story = new Story();
+		       story.setTitle(input.getText().toString());
+		       Random rg = new Random();
+		       int rint = rg.nextInt(100);
+		       
+		       story.setId(story.getTitle()+rint);
+		       story.setAuthor(Uname);
+		       JSONparser jp = new JSONparser();
+		       
+		       try {
+				jp.storeStory(story);
+			} catch (IllegalStateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		       }  
+		     });  
+		
+		adb.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+	        public void onClick(DialogInterface dialog, int which) {
+	           
+	            return;   
+	        }
+	    });
+	
+	
+	}
+		
+		
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.

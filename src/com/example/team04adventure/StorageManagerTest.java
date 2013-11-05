3396d7e459d276aba45b35b25963635015a5fdb2
mkmicik <Mike@uwsclient-146-2.uws.ualberta.ca>
2013-11-05 13:32:37 -0700

    fds

--- src/com/example/team04adventure/StorageManagerTest.java
+++ src/com/example/team04adventure/StorageManagerTest.java
@@ -1,5 +1,24 @@
 package com.example.team04adventure;
 
-public class StorageManagerTest {
+/**
+ * @author Team04Adventure
+ * The J-unit test for the storage manager. It tests all queries of the
+ * SQL tables, as well as storing and deleting tuples.
+ *
+ */
 
+public class StorageManagerTest implements TestCase {
+
+	public void setUp(){
+		
+	}
+	
+	public void tearDown() {
+		
+	}
+	
+	public void run(){
+		
+	}
+	
 }

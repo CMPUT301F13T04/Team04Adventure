c5de25e26cd6d6ede07a13adc2addf0f99e4674e
hassaanali1 <hassaan1@ualberta.ca>
2013-11-25 17:31:19 -0700

    Update StoryListAdapter.java

--- src/com/example/team04adventure/Model/StoryListAdapter.java
+++ src/com/example/team04adventure/Model/StoryListAdapter.java
@@ -15,7 +15,6 @@
 
 package com.example.team04adventure.Model;
 
-
 import java.util.ArrayList;
 
 import android.content.Context;
@@ -30,79 +29,83 @@ import com.example.team04adventure.R;
 
 /**
  * StoryListAdapter is an adapter to get the story list.
+ * 
  * @author Team04Adventure
  */
 public class StoryListAdapter extends BaseAdapter {
- 
-		private ArrayList<Story> stories;
-    	ArrayList<Story> storiesClone;
-	 
-	    private LayoutInflater layoutInflater;
-	 
-	    public StoryListAdapter(Context context, ArrayList<Story> stories) {
-	        this.stories = stories;
-	        layoutInflater = LayoutInflater.from(context);
-	        storiesClone = new ArrayList<Story>();
-	        storiesClone.addAll(stories);
-	    }
-	 
-	    @Override
-	    public int getCount() {
-	    	return stories.size();
-	    }
-	 
-	    @Override
-	    public Object getItem(int position) {
-	        return stories.get(position);
-	    }
-	 
-	    @Override
-	    public long getItemId(int position) {
-	        return position;
-	    }
-	 
-	    public View getView(int position, View convertView, ViewGroup parent) {
-	        ViewHolder holder;
-	        if (convertView == null) {
-	            convertView = layoutInflater.inflate(R.layout.storylistlayout, null);
-	            holder = new ViewHolder();
-	            holder.titleView = (TextView) convertView.findViewById(R.id.storytitle);
-	            holder.authorView = (TextView) convertView.findViewById(R.id.storyauthor);
-	            holder.fragmentView = (TextView) convertView.findViewById(R.id.storyfragments);
-	            
-	            convertView.setTag(holder);
-	        } else 
-	            holder = (ViewHolder) convertView.getTag();
-	        
-	        holder.titleView.setText(stories.get(position).getTitle());
-	        holder.authorView.setText("By: " + stories.get(position).getAuthor());
-	        holder.fragmentView.setText("Fragments: " + stories.get(position).getFrags().size());
-	        
-	 
-	        return convertView;
-	    }
-	 
-	    static class ViewHolder {
-	       
-	    	ImageView profilePic;
-	    	TextView titleView;
-	        TextView authorView;
-	        TextView fragmentView;
-	      
-	    }
-	    
-	    public void filter(String query) {
-            if (query != null) {
-                    stories.clear();
-                    for (Story s : storiesClone) {
-                            String searchString = s.getTitle().concat(" ").concat(s.getAuthor());
-                            if(searchString.matches("(?i)(.*)"+query+"(.*)")) {
-                                    this.stories.add(s);
-                            }
-                            }
-                    this.notifyDataSetChanged();
-            }
-    }
 
-	 
+	private ArrayList<Story> stories;
+	ArrayList<Story> storiesClone;
+
+	private LayoutInflater layoutInflater;
+
+	public StoryListAdapter(Context context, ArrayList<Story> stories) {
+		this.stories = stories;
+		layoutInflater = LayoutInflater.from(context);
+		storiesClone = new ArrayList<Story>();
+		storiesClone.addAll(stories);
+	}
+
+	@Override
+	public int getCount() {
+		return stories.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return stories.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder holder;
+		if (convertView == null) {
+			convertView = layoutInflater
+					.inflate(R.layout.storylistlayout, null);
+			holder = new ViewHolder();
+			holder.titleView = (TextView) convertView
+					.findViewById(R.id.storytitle);
+			holder.authorView = (TextView) convertView
+					.findViewById(R.id.storyauthor);
+			holder.fragmentView = (TextView) convertView
+					.findViewById(R.id.storyfragments);
+
+			convertView.setTag(holder);
+		} else
+			holder = (ViewHolder) convertView.getTag();
+
+		holder.titleView.setText(stories.get(position).getTitle());
+		holder.authorView.setText("By: " + stories.get(position).getAuthor());
+		holder.fragmentView.setText("Fragments: "
+				+ stories.get(position).getFrags().size());
+
+		return convertView;
+	}
+
+	static class ViewHolder {
+
+		ImageView profilePic;
+		TextView titleView;
+		TextView authorView;
+		TextView fragmentView;
+
+	}
+
+	public void filter(String query) {
+		if (query != null) {
+			stories.clear();
+			for (Story s : storiesClone) {
+				String searchString = s.getTitle().concat(" ")
+						.concat(s.getAuthor());
+				if (searchString.matches("(?i)(.*)" + query + "(.*)")) {
+					this.stories.add(s);
+				}
+			}
+			this.notifyDataSetChanged();
+		}
+	}
 }
